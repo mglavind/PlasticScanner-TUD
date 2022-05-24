5023d13f8b4a173c25b0bf1b4a72489c851b860e
Markus Glavind <glavindmarkus@gmail.com>
2022-05-24 20:49:06 +0200

    Deep sleep + trigger wakeup

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,29 @@
         pio run --target upload && pio device monitor
 */
 #include "assert.h"
-#include "cli.h"
 #include <ADS1256.h>
 #include <tlc59208.h>
 #include <Arduino.h>
 #include <WiFi.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <WiFiMulti.h>
 
+WiFiMulti wifiMulti;
 
 const int buttonPin = 33;
 int buttonState = 0;
+
+// Deep sleep
+#define BUTTON_PIN_BITMASK 0x200000000 // 2^33 in hex
+RTC_DATA_ATTR int bootCount = 0;
+unsigned long LastUseMillis = 0;
+unsigned long SleepTimer = 100000;
+unsigned long currentMillis = millis();
+
+// For timekeeping of Wifi Reconnect
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
 
 
 /*
@@ -22,14 +34,6 @@ int buttonState = 0;
             Setup to Google sheets start
     /////////////////////////////////////////////////
 */
-
-
-
-// WiFi SSID and Password
-//const char* ssid     = "Biosphere";
-//const char* password = "pl4stic-sc4nner";
-const char* ssid     = "Markus iPhone";
-const char* password = "KomNuMand";
 
 // IFTTT URL resource
 const char* resource = "/trigger/PlasticScanned/with/key/bl96IM25tg14213NBlSzwH";
@@ -49,11 +53,9 @@ float vRef = 2.5; // voltage reference
 ADS1256 adc(clockMHZ,vRef,false); // RESETPIN is permanently tied to 3.3v
 //ADS1256 adc(CLKSPEED_MHZ, VREF, false); // this line is cilling the script
 TLC59208 ledctrl;
-Cli cli;
 
-void SerialClient() {
-    cli.handle();
-}
+
+
 
 void read_adc(int argc, char *argv[])
 {
@@ -63,32 +65,8 @@ void read_adc(int argc, char *argv[])
 }
 
 
-/*
-void scan(int argc, char *argv[])
-{
-    float preScan = adc.readCurrentChannel();   // making a scan without LED's
 
-    float readings[8] = {0};
-    for (int i=0; i<8; i++) {                   // taking 8 scan values
-        ledctrl.on(i);                          // turning LED "i" on
-        delay(5);                               // wait for it to be stable
-        adc.waitDRDY();                         // Check if sensor is ready 
-        readings[i] = adc.readCurrentChannel(); // Read sensor and save in place "i"
-        ledctrl.off(i);                         // turn off LED "i"
-    }
-
-
-    float postScan = adc.readCurrentChannel();  // making a scan without LED's
-    
-    for (int i=0; i<8; i++) {       
-        Serial.print(readings[i], 5);           // this function just prints all the readings
-        Serial.print('\t');                     // adds space (tap) between all values
-    }
-    Serial.println();
-
-    ////////////////////////////////////////
-    //     Transmission to google sheets
-    ////////////////////////////////////////
+void sendData(String data){
     Serial.print("Connecting to "); 
     Serial.print(server);
     
@@ -98,58 +76,44 @@ void scan(int argc, char *argv[])
         Serial.print(".");
     }
     Serial.println();
+    
     if(!!!client.connected()) {
         Serial.println("Failed to connect...");
     }
-    // All readings to one string
-    int multiplier = 10000000 ;
-    String ResultsString = String(readings[0]*multiplier) + ";" + 
-            String(readings[1]*multiplier) + ";" + 
-            String(readings[2]*multiplier) + ";" +
-            String(readings[3]*multiplier) + ";" +
-            String(readings[4]*multiplier) + ";" +
-            String(readings[5]*multiplier) + ";" +
-            String(readings[6]*multiplier) + ";" +
-            String(readings[7]*multiplier) + ";" +
-            String(preScan*multiplier)     + ";" +
-            String(postScan*multiplier)
-            ; 
 
-    // string as Json object
-    String jsonObject = String("{\"value1\":\"") + (ResultsString) +  "\"}";
-
-    client.println(String("POST ") + resource + " HTTP/1.1");
-    client.println(String("Host: ") + server); 
-    client.println("Connection: close\r\nContent-Type: application/json");
-    client.print("Content-Length: ");
-    client.println(jsonObject.length());
-    client.println();
-    client.println(jsonObject);
-            
-    int timeout = 5 * 10; // 5 seconds             
-    while(!!!client.available() && (timeout-- > 0)){
-        delay(100);
+    if(wifiMulti.run() == WL_CONNECTED) {    
+        client.println(String("POST ") + resource + " HTTP/1.1");
+        client.println(String("Host: ") + server); 
+        client.println("Connection: close\r\nContent-Type: application/json");
+        client.print("Content-Length: ");
+        client.println(data.length());
+        client.println();
+        client.println(data);
+                
+        int timeout = 5 * 10; // 5 seconds             
+        while(!!!client.available() && (timeout-- > 0)){
+            delay(100);
+        }
+        if(!!!client.available()) {
+            Serial.println("No response...");
+        }
+        while(client.available()){
+            Serial.write(client.read());
+        }
+        Serial.println("\nclosing connection");
+        client.stop();
     }
-    if(!!!client.available()) {
-        Serial.println("No response...");
+    else{
+        Serial.println("Not Connected to WiFi");
+        Serial.println("Would have sent the following data:");
+        Serial.println(data);
     }
-    while(client.available()){
-        Serial.write(client.read());
-    }
-    
-    Serial.println("\nclosing connection");
-    client.stop();
-    
-    SerialClient();
-    ////////////////////////////////////////
-    //     Transmission to google sheets
-    ////////////////////////////////////////
 }
-*/
+
 void scan()
 {
+    // Place "scanning" screen here
     float preScan = adc.readCurrentChannel();   // making a scan without LED's
-
     float readings[8] = {0};
     for (int i=0; i<8; i++) {                   // taking 8 scan values
         ledctrl.on(i);                          // turning LED "i" on
@@ -171,18 +135,7 @@ void scan()
     ////////////////////////////////////////
     //     Transmission to google sheets
     ////////////////////////////////////////
-    Serial.print("Connecting to "); 
-    Serial.print(server);
-    
-    WiFiClient client;
-    int retries = 5;
-    while(!!!client.connect(server, 80) && (retries-- > 0)) {
-        Serial.print(".");
-    }
-    Serial.println();
-    if(!!!client.connected()) {
-        Serial.println("Failed to connect...");
-    }
+
     // All readings to one string
     int multiplier = 10000000 ;
     String ResultsString = String(readings[0]*multiplier) + ";" + 
@@ -200,32 +153,8 @@ void scan()
     // string as Json object
     String jsonObject = String("{\"value1\":\"") + (ResultsString) +  "\"}";
 
-    client.println(String("POST ") + resource + " HTTP/1.1");
-    client.println(String("Host: ") + server); 
-    client.println("Connection: close\r\nContent-Type: application/json");
-    client.print("Content-Length: ");
-    client.println(jsonObject.length());
-    client.println();
-    client.println(jsonObject);
-            
-    int timeout = 5 * 10; // 5 seconds             
-    while(!!!client.available() && (timeout-- > 0)){
-        delay(100);
-    }
-    if(!!!client.available()) {
-        Serial.println("No response...");
-    }
-    while(client.available()){
-        Serial.write(client.read());
-    }
-    
-    Serial.println("\nclosing connection");
-    client.stop();
-    
-    SerialClient();
-    ////////////////////////////////////////
-    //     Transmission to google sheets
-    ////////////////////////////////////////
+    // sends data
+    sendData(jsonObject);
 }
 
 
@@ -254,47 +183,34 @@ void led(int argc, char *argv[])
 }
 
 
-
-void help(int argc, char *argv[])
-{
-    cli.list_commands();
-}
-
-
 // Establish a Wi-Fi connection with your router
 void initWifi() {
-  Serial.print("Connecting to: "); 
-  Serial.print(ssid);
-  WiFi.begin(ssid, password);  
+    wifiMulti.addAP("Markus iPhone", "KomNuMand");
+    wifiMulti.addAP("Biosphere", "pl4stic-sc4nner");
+    wifiMulti.addAP("TSH Guest", "");
 
-  int timeout = 10 * 4; // 10 seconds
-  while(WiFi.status() != WL_CONNECTED  && (timeout-- > 0)) {
-    delay(250);
-    Serial.print(".");
-  }
-  Serial.println("");
-
-  if(WiFi.status() != WL_CONNECTED) {
-     Serial.println("Failed to connect, going back to sleep");
-  }
-
-  Serial.print("WiFi connected in: "); 
-  Serial.print(millis());
-  Serial.print(", IP address: "); 
-  Serial.println(WiFi.localIP());
+    Serial.println("Connecting Wifi...");
+    if(wifiMulti.run() == WL_CONNECTED) {
+        Serial.println("");
+        Serial.println("Connected to WiFi:");
+        Serial.println(WiFi.SSID());
+    }
+    else{
+        Serial.println("WiFi Failed. Trying again in 30sec");
+    }
 }
 
 
 void setup()
 {
-
     Serial.begin(9600);
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_33,0); //1 = High, 0 = Low
     Serial.println("Serial Up");
     SPI.begin();
     SPI.beginTransaction(
         SPISettings(clockMHZ * 1000000 / 4, MSBFIRST, SPI_MODE1));
-
     Serial.println("SPI Up");
+    // Place Booting screen here
     Wire.begin();
     Serial.println("Wire Up");
     ledctrl.begin();
@@ -302,16 +218,10 @@ void setup()
     adc.begin(ADS1256_DRATE_30000SPS,ADS1256_GAIN_1,false); 
     Serial.println("ADC Up");
     adc.setChannel(0,1);    // differential ADC reading 
-
     initWifi();
-    /*
-    cli.add_command({"scan", scan, "Perform a scan sequence: for each led measure adc value"});
-    cli.add_command({"adc", read_adc, "Reads ADC measurement"});
-    cli.add_command({"led", led, "Turns an LED <number> on/off <state>.\n\t\t\t\tUsage: led <number> <state>"});
-    cli.add_command({"help", help, "Lists all available commands"});
-    cli.begin();
-    */
+
     pinMode(buttonPin, INPUT);
+    LastUseMillis = millis();
     Serial.println("Trigger Up");
     Serial.println("PlasticScanner is initialized!");
 }
@@ -321,16 +231,31 @@ void setup()
 
 void loop()
 {
-    //cli.handle();
-
     buttonState = digitalRead(buttonPin);
+    currentMillis = millis();
+
     if (buttonState == LOW) {
         scan();
+        LastUseMillis = currentMillis;
+
     } 
-    else{
-        
+    // Place result screen here
+
+
+    // if WiFi is down, try reconnecting
+    if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= interval)) {
+        Serial.print(millis());
+        Serial.println("Reconnecting to WiFi...");
+        WiFi.disconnect();
+        initWifi();
+        previousMillis = currentMillis;
     }
-
-
+    if (currentMillis - LastUseMillis >= SleepTimer)
+    {
+        Serial.println("Going to sleep...");
+        // Place "Going to sleep screen" + delay here
+        esp_deep_sleep_start();
+    }
+    
 }
 
